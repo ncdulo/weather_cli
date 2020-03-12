6b1e39ecfb73bf1bc1b143df6d46fbc9ccaf598e
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-03-12 16:58:41 -0400

    Initialize repo.
    `weather_cli` is a simple toy project extending upon a tutorial program
    indended to show off working with `click`. This program provides a
    simple CLI for pulling current weather conditions for a specified
    location. This initial revision represents the tutorial program with
    some minor changes to output formatting.
    
    This code was put together based on the following guides:
    
      * https://dbader.org/blog/python-commandline-tools-with-click
      * https://dbader.org/blog/mastering-click-advanced-python-command-line-apps

---
+++ cli.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import os
+import re
+import click
+import requests
+
+
+class ApiKey(click.ParamType):
+    name = 'api-key'
+
+    def convert(self, value, param, ctx):
+        found = re.match(r'[0-9a-f]{32}', value)
+
+        if not found:
+            self.fail(
+                    f'{value} is not a 32-character hexadecimal string',
+                    param,
+                    ctx,
+                )
+
+        return value
+
+
+def current_weather(location, api_key):
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+
+    query_params = {
+            # If using City ID, set the 'id' parameter
+            #'id': location,
+            # Otherwise, we are using City names, 'q' parameter
+            'q': location,
+            'units': 'imperial',
+            'appid': api_key,
+        }
+
+    response = requests.get(url, params=query_params)
+
+    #current_conditions =  response.json()['weather'][0]['description'] + \
+    #        ' at ' + \
+    #        str(response.json()['main']['temp']) + '°F' + \
+    #        ' (' + \
+    #        str(response.json()['main']['temp_max']) + '°F High, ' + \
+    #        str(response.json()['main']['temp_min']) + '°F Low) at ' +\
+    #        str(response.json()['main']['humidity']) + '% humidity'
+
+    current_conditions = \
+            response.json()['weather'][0]['description'] + \
+            ', ↑' + str(response.json()['main']['temp_max']) + '°F, ' + \
+            str(response.json()['main']['temp']) + '°F, ↓' + \
+            str(response.json()['main']['temp_min']) + '°F, ' + \
+            str(response.json()['main']['humidity']) + '%RH'
+
+    return current_conditions
+
+
+@click.group()
+@click.option(
+        '--api-key', '-a',
+        type=ApiKey(),
+        help='Your OpenWeatherMap API key')
+@click.option(
+        '--config-file', '-c',
+        type=click.Path(),
+        default='~/.weather.cfg')
+@click.pass_context
+def main(ctx, api_key, config_file):
+    '''
+    A simple weather tool that shows the current weather condition in a
+    LOCATION of your choice. Provide the city name, and optionally a two-digit
+    country code. If the city name contains spaces, it should be enclosed in
+    quotation marks. Examples:
+
+    1. "New York City,US"
+
+    2. Berlin
+
+    You need a valid API key from OpenWeatherMap for this tool to work. You
+    can sign up for a free account at https://openweathermap.org/appid.
+    '''
+    filename = os.path.expanduser(config_file)
+
+    if not api_key and os.path.exists(filename):
+        with open(filename) as cfg:
+            api_key = cfg.read()
+
+    ctx.obj = {
+            'api_key': api_key,
+            'config_file': filename,
+        }
+
+
+@main.command()
+@click.pass_context
+def config(ctx):
+    '''
+    Store the OpenWeatherMap API key in configuration file.
+    '''
+    config_file = ctx.obj['config_file']
+
+    api_key = click.prompt(
+            'Please enter your OpenWeatherMap API key',
+            default=ctx.obj.get('api_key', '')
+        )
+
+    with open(config_file, 'w') as cfg:
+        cfg.write(api_key)
+
+
+@main.command()
+@click.argument('location')
+@click.pass_context
+def current(ctx, location):
+    '''
+    Show the current weather for a location using OpenWeatherMap data.
+    '''
+    api_key = ctx.obj['api_key']
+
+    weather = current_weather(location, api_key)
+    print(f'Current conditions for  {location}: {weather}.')
+
+if __name__ == '__main__':
+    main()
