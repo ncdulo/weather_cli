e9d0573e052a620a7d0d25736a60c12b90411598
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-03-14 20:48:22 -0400

    Implement proper output templates via `--pretty`.
    This commit brings in the functionality for our new `--pretty` output
    formats. We start off by defining a series of output templates, and pick
    one based on the user specied arguments.
    
    Then, as a slight modification to our old string concatenation Hell, we
    build up a dictionary of the current conditions. That dictionary gets
    passed into `Template.safe_substitute()` for string replacement before
    finally displaying the data to the user.
    
    We use `safe_substitute()` to allow missing template keys to be skipped
    over without an error. Goes in hand with our key checks in the code
    assembling the dictionary.

--- cli.py
+++ cli.py
@@ -152,24 +152,35 @@ def current(ctx, location, units, pretty, conditions, temperature, humidity,\
         print(f'Timed Out!\n{error}')
         return
 
-    # Vestigial code. Probably do not need any longer.
-    current_conditions = ''
+    current_conditions = {'location': location,}
 
     # Choose an output template based on the chosen option
     if pretty == 'short':
-        template = Template('short')
+        # Default, short & to the point
+        template = Template(
+'''$conditions $temperature $humidity $wind'''
+            )
     elif pretty == 'long':
-        template = Template('long')
+        # Longer form, additional formatting
+        template = Template(
+'''Current conditions for ${location}:
+    $conditions $temperature $humidity $wind'''
+            )
     elif pretty == 'verbose':
-        template = Template('verbose')
-
-    if pretty:
-        current_conditions += f'Current conditions for {location}: '
+        # Describe the full output
+        # TODO: Pretty print into a table!
+        template = Template(
+'''Location: $location
+Conditions: $conditions
+Temperature (High, Current, Low): $temperature
+Relative Humidity: $humidity
+Wind (Direction, Speed): $wind'''
+            )
 
     if conditions:
         if 'weather' in weather:
-            current_conditions += \
-                    weather['weather'][0]['description'].capitalize() + ', '
+            current_conditions['conditions'] = \
+                    weather['weather'][0]['description'].capitalize()
 
     if temperature:
         if 'temp_max' in weather['main'] and \
@@ -180,14 +191,15 @@ def current(ctx, location, units, pretty, conditions, temperature, humidity,\
                     'metric': 'C',
                     'standard': 'K',
                 }
-            current_conditions += '↑' + \
+            current_conditions['temperature'] = '↑' + \
                     str(weather['main']['temp_max']) + f'°{temp_units[units]}, ' +\
                     str(weather['main']['temp']) + f'°{temp_units[units]}, ↓' + \
-                    str(weather['main']['temp_min']) + f'°{temp_units[units]}, '
+                    str(weather['main']['temp_min']) + f'°{temp_units[units]}'
 
     if humidity:
         if 'humidity' in weather['main']:
-            current_conditions += str(weather['main']['humidity']) + '%RH, '
+            current_conditions['humidity'] = \
+                    str(weather['main']['humidity']) + '%RH'
 
     if wind:
         if 'wind' in weather:
@@ -198,16 +210,14 @@ def current(ctx, location, units, pretty, conditions, temperature, humidity,\
                         'standard': 'm/s',
                     }
                 wind_cardinal = degrees_to_cardinal(weather['wind']['deg'])
-                current_conditions += f'winds {wind_cardinal} at ' + \
+                current_conditions['wind'] = \
+                        f'winds {wind_cardinal} at ' + \
                         str(weather['wind']['speed']) + f'{wind_units[units]}'
-
-    # Original output call becomes debug text. Remove this.
-    print(current_conditions)
 
     # Pass our conditions dictionary into the 'pretty' template and
     # substitue our weather data in for display.
-    current_conditions = template.safe_substitute()
-    print(current_conditions)
+    output_text = template.safe_substitute(**current_conditions)
+    print(output_text)
 
 if __name__ == '__main__':
     main()
